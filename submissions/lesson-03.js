222e036b2c3f89b3cac6d29309087756924fb8f1
bamonline1123 <bamkelly1123@gmail.com>
2026-09-02 19:21:26 -0400

    Update lesson-03.js

--- submissions/lesson-03.js
+++ submissions/lesson-03.js
@@ -21,17 +21,17 @@ function main(k) {
 // ──────────────────────────────────────────────────────────
 function problem_2() {
 function main(k) {
+function scan(k) {
   if (k.beepersPresent()) {
-    k.pickBeeper();
+    k.pickBeeper()
   }
-  k.move();
-  if (k.beepersPresent()) {
-    k.pickBeeper();
+  if (k.frontIsClear()) {
+    k.move();
   }
-  k.move();
-  if (k.beepersPresent()) {
-    k.pickBeeper();
-  }
+}
+  scan(k);
+  scan(k);
+  scan(k);
 }
   return main;
 }
@@ -41,21 +41,85 @@ function main(k) {
 // ──────────────────────────────────────────────────────────
 function problem_3() {
 function main(k) {
+function scan(k) {
   if (k.beepersPresent()) {
-    k.paintCorner("Red");
+    k.paintCorner("Red")
   }
-  k.move();
-  if (k.beepersPresent()) {
-    k.paintCorner("Red");
+  if (k.frontIsClear()) {
+    k.move();
   }
-  k.move();
-  if (k.beepersPresent()) {
-    k.paintCorner("Red");
+}
+  scan(k);
+  scan(k);
+  scan(k);
+  scan(k);
+}
+  return main;
+}
+
+// ──────────────────────────────────────────────────────────
+// Problem 4: complex2 (Complex II)
+// ──────────────────────────────────────────────────────────
+function problem_4() {
+function main(k) {
+function turnRight(k) {
+  k.turnLeft();
+  k.turnLeft();
+  k.turnLeft();
+}
+function scan(k) {
+  if (k.cornerColorIs("Blue")) {
+    k.putBeeper();
+    k.putBeeper();
+    k.putBeeper();
   }
-  k.move();
-  if (k.beepersPresent()) {
-    k.paintCorner("Red");
+  if (k.cornerColorIs("Orange")) {
+    k.putBeeper();
+    k.putBeeper();
+  } 
+  if (k.cornerColorIs("Red")) {
+    k.putBeeper();
   }
+  if (k.frontIsClear()) {
+    k.move()
+  }
+}
+
+function RowAcross(k) {
+  scan(k);
+  scan(k);
+  scan(k);
+  scan(k);
+  scan(k);
+  scan(k);
+  scan(k);
+  scan(k);
+}
+
+function HeadRight(k) {
+  RowAcross(k);
+  k.turnLeft();
+  if (k.frontIsClear()) {
+    k.move(); 
+  }
+  k.turnLeft();
+}
+function HeadLeft(k) {
+  RowAcross(k);
+  turnRight(k);
+  if (k.frontIsClear()) {
+    k.move();
+  }
+  turnRight(k);
+}
+HeadRight(k);
+HeadLeft(k);
+HeadRight(k);
+HeadLeft(k);
+HeadRight(k);
+HeadLeft(k);
+HeadRight(k);
+HeadLeft(k);
 }
   return main;
 }
